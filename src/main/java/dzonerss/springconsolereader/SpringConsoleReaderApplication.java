0ab5c917c62dc29c754525596dedeb12060ffad3
Tamas Szabo <Tamas_Szabo5@epam.com>
2017-09-29 11:26:58 +0200

    Initial commit

---
+++ src/main/java/dzonerss/springconsolereader/SpringConsoleReaderApplication.java
@@ -0,0 +1,12 @@
+package dzonerss.springconsolereader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConsoleReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConsoleReaderApplication.class, args);
+	}
+}
---
+++ src/test/java/dzonerss/springconsolereader/SpringConsoleReaderApplicationTests.java
@@ -0,0 +1,16 @@
+package dzonerss.springconsolereader;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringConsoleReaderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
